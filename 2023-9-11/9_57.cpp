c68ba18cc437561dc53ee8a10173eaeb47765b00
HRLBZE <1214899196@qq.com>
2023-09-11 12:18:53 +0800

    添加项目文件。

---
+++ 2023-9-11/9_57.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+//int transform(int);     //括号内如果是空号则说明不需要输入
+//int main()
+//{
+//	using namespace std;
+//	cout << "please input the distance:";
+//	int Distance;
+//	cin >> Distance;
+//	cout /*<< endl*/ << "The transform distance is:" << transform(Distance);
+//	return 0;
+//}
+//int transform(int n)
+//{
+//	int Distance = n * 220;
+//	return Distance;
+//}
