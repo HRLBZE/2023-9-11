272488d0bd8254439eee3030647cd21cfcdf103c
HRLBZE <1214899196@qq.com>
2023-09-12 16:46:13 +0800

    2023-9-12 unit three

--- 2023-9-11/9_57.cpp
+++ 2023-9-11/9_57.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
+#include <climits>
 using namespace std;
+//int main()
+//{
+//	cout << "Enter the number of hours:";
+//	int hours;
+//	cin >> hours;
+//	cout << "Enter the number of minutes:";
+//	int minutes;
+//	cin >> minutes;
+//	cout << "Time:" << hours << ":" << minutes;
+//	return 0;
+//}
+int main()
+{
+	char M = 'L';
+	int a = M;
+	cout << M << endl << a;
+	cout << endl;
+	cout.put(M);
+	return 0;
+	/*int a = 27;
+	cout << oct;
+	cout << a << endl;
+	cout << hex;
+	cout << a << endl;
+	cout << dec;
+	cout << a << endl;
+	return 0;*/
+	//int a{2};
+	//cout << a;
+	//cout << "yiu";
+	//return 0;
+	//
+	//int n_int = INT_MAX;
+	//short n_short = SHRT_MAX;
+	//cout << n_int << endl;
+	//cout << n_short << endl;
+	//cout << sizeof(int);
+	//return 0;
+}
 
 
 
-
+//#include <iostream>
+//using namespace std;
+//
+//void One()
+//{
+//	cout << "see how they run" << endl;
+//}
+//
+//void Second();
+//
+//int main()
+//{
+//	cout << "I will output something";
+//	cout << endl;
+//	Second();
+//	Second();
+//	One();
+//	One();
+//	return 0;
+//}
+//
+//void Second()
+//{
+//	cout << "Three blind mice" << endl;
+//}
 //int transform(int);     //括号内如果是空号则说明不需要输入
 //int main()
 //{
